ba338f6e41f121d630d70046428d0895ac345c6c
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-06-10 10:28:51 +0300

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,9 @@
-import { PageRouteProps } from "../interface";
-import Button from "../components/common/Button";
+import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import { FaHome } from 'react-icons/fa';
+
+interface PageRouteProps {
+  pageRoute: string
+}
 
 export default function Home() {
   const router = useRouter()
@@ -14,8 +16,9 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
-    
-      <h1> Welcome to Splash App </h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome to Splash App!
+      </h1>
       <p className="text-lg text-gray-600 mb-8">
         Your one-stop platform for everything AI you need. Start exploring by
         navigating to our features below.
